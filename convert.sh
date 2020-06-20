3d5d3df1c08338aed77a1c919ba58effa62ba2f4
David Bonnici <david.bonnici@quickspin.com>
2020-06-20 09:43:07 +0200

    Adding checks before deleting previously generated files.

--- convert.sh
+++ convert.sh
@@ -3,11 +3,15 @@ dirs=$(find /files -name '*.drawio')
 
 for dir in $dirs
 do
-    echo "Converting ${dir} to ${dir}.pdf" 
-    rm $dir.pdf
+    echo "Converting ${dir} to ${dir}.pdf"
+    if test -f "${dir}.pdf"; then
+        rm $dir.pdf
+    fi
     node /home/node/draw.io-export/bin/drawio.js $dir -o $dir.pdf
     echo "Converting ${dir} to ${dir}.png" 
-    rm $dir.png
+    if test -f "${dir}.png"; then
+        rm $dir.png
+    fi
     node /home/node/draw.io-export/bin/drawio.js $dir -o $dir.png
 done
 echo "DRAW.IO-EXPORT Finished."
