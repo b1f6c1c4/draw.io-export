5db143947285737b556e209dc63f118e27e69047
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-07-17 17:12:48 -0400

    fix minor coding style problems

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM node:lts-alpine3.11
 RUN apk add chromium
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
 ENV CHROMIUM_PATH /usr/bin/chromium-browser
-RUN mkdir -p /home/node/draw.io-export /files 
+RUN mkdir -p /home/node/draw.io-export /files
 WORKDIR /home/node/draw.io-export
 COPY . .
 RUN npm install
 VOLUME ["/files"]
-ENTRYPOINT [ "sh", "convert.sh" ]
+ENTRYPOINT ["sh", "convert.sh"]
--- convert.sh
+++ convert.sh
@@ -1,17 +1,15 @@
+#!/bin/bash
+
 echo "DRAW.IO-EXPORT Started."
 dirs=$(find /files -name '*.drawio')
 
 for dir in $dirs
 do
     echo "Converting ${dir} to ${dir}.pdf"
-    if test -f "${dir}.pdf"; then
-        rm $dir.pdf
-    fi
-    node /home/node/draw.io-export/bin/drawio.js $dir -o $dir.pdf
-    echo "Converting ${dir} to ${dir}.png" 
-    if test -f "${dir}.png"; then
-        rm $dir.png
-    fi
-    node /home/node/draw.io-export/bin/drawio.js $dir -o $dir.png
+    rm -f "$dir.pdf"
+    node /home/node/draw.io-export/bin/drawio.js "$dir" -o "$dir.pdf"
+    echo "Converting ${dir} to ${dir}.png"
+    rm -f "$dir.png"
+    node /home/node/draw.io-export/bin/drawio.js "$dir" -o "$dir.png"
 done
 echo "DRAW.IO-EXPORT Finished."
