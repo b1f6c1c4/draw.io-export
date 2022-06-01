a3c69dd71c81edccdbefee38c39062c42b71cda6
b1f6c1c4 <b1f6c1c4@gmail.com>
2022-06-01 02:27:49 -0400

    cp --attributes-only in docker
    Fixes #5.

--- convert.sh
+++ convert.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cp --attributes-only "$1" "$1.pdf"
+cp --attributes-only "$1" "$1.png"
+
 printf "Converting %s to %s\n" "$1" "$1.pdf"
 rm -f "$1.pdf"
 node ./bin/drawio.js "$1" -o "$1.pdf"
